931d6b087e1697f5f9c9418339e4cbc4096abd3e
Seaflight4 <161011443+Seaflight4@users.noreply.github.com>
2024-04-20 11:49:40 +0800

    Update main.cpp

--- GP/main.cpp
+++ GP/main.cpp
@@ -61,11 +61,7 @@ void screenSetUp()
 	keypad(stdscr, true);
 	refresh();
 
-    bkgd(COLOR_PAIR(5));
     start_color();
-    init_pair(5, COLOR_WHITE, COLOR_WHITE);
-
-
     init_pair(0, COLOR_RED, COLOR_BLACK);
     init_pair(1, COLOR_GREEN, COLOR_BLACK);
     init_pair(2, COLOR_YELLOW, COLOR_BLACK);

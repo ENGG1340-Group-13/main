24c6aa4d3222ae6328dc059a39937b82df989cb1
guoxinyicindy <161011348+guoxinyicindy@users.noreply.github.com>
2024-04-20 11:29:22 +0800

    Update screenwin.cpp

--- GP/screenwin.cpp
+++ GP/screenwin.cpp
@@ -58,9 +58,13 @@ void ScreenWin::draw_partscreen(int left,int width,int linecnt,vector<string> &t
 void ScreenWin::EndWin()
 {
     cout << "\033[2J\033[1;1H"<<endl;
-    tcsetattr(STDIN_FILENO, TCSANOW, &old_tio);
     cout<<"Press Enter to continue"<<endl;
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF) { }
+    char c;
+    c = getchar(); 
+    while (c != '\n' && c != EOF) { 
+        c = getchar(); 
+        } 
+    cout << "\033[2J\033[1;1H"<<endl;
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_tio);
 }
 

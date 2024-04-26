0b42abae459fc7bb2a4ca38562c0442376c1daa5
Booker-zgd <161011007+Booker-zgd@users.noreply.github.com>
2024-04-26 15:22:42 +0800

    Update maze.h

--- GP/maze.h
+++ GP/maze.h
@@ -1,5 +1,3 @@
-/*这个类用于在内存中构建一个和屏幕控制台一样大小的"内存屏幕"，存储地图（map）以及闯关者（warrior)
-当前的状态，以便和实际的屏幕显示逻辑进行分离。因为屏幕显示逻辑需要有跨平台的不同版本*/
 #pragma once
 
 #include <stdio.h>
@@ -11,10 +9,11 @@
 #include "screenwin.h"
 #include "keyboardwin.h"
 
-
+//define the name of map files.
 #define MAPFILENAME1 "map3.txt"
 #define MAPFILENAME2 "map4.txt"
 
+//define the name of keys
 #define MAP_CHAR 'X'
 #define MAP_EXIT 'E'
 #define OBSTACLE 'O'
@@ -22,34 +21,41 @@
 #define WARRIOR_DIE '!'
 #define WARRIOR_OUT '#'
 
+//define the status of map.
 #define MAP_POS_X 0
 #define MAP_POS_Y 0
 
+//define the name of colors.
 #define RED "31m"
 #define BLUE "34m"
 #define GREEN "32m"
 #define DEFAULT_COLOR "0m"
 
+//define the speed
 #define SPEED 250000000
 
+//define the struct of map, including the position and vector of the map.
 struct Mem_Map{
-    int map_x;//地图在屏幕上左上角起始点的横坐标 
-    int map_y;//地图在屏幕上左上角起始点的纵坐标
-    int lines;//地图图形的行数
-    int map_width;//地图的宽度 
+    int map_x;
+    int map_y;
+    int lines;
+    int map_width;
     vector<string> mem_map;  
 };
 
+//define the struct of the player, with x and y positions.
 struct Mem_Warrior{
     int warrior_x;
     int warrior_y;
 };
 
+//define the struct of the exit, with the x and y positions.
 struct Mem_Exit{
     int exit_x;
     int exit_y;
 };
 
+//define the class of obstacles, with the positions, directions of the obstacles, and the functions to handle the obstacles.
 class Mem_Obstacle{
 public:
     int obstacle_x;
@@ -66,18 +72,18 @@ private:
 	void generate_obs();
     void updatechar(int xpos,int ypos,char tmpicon, string color);    
 public:
-    Mem_Map mymap;//游戏中的“地图” 
-    Mem_Warrior mywarrior;//游戏中的“勇士” 
-    Mem_Exit myexit;//游戏中的“出口” 
-    vector<Mem_Obstacle> myobstacles;//游戏中的所有“障碍物” 
-    ScreenWin screen;//游戏中控制“控制台”显示的对象 
-    KeyboardWin keyboard;//游戏中读取键盘输入的对象     
+    Mem_Map mymap;
+    Mem_Warrior mywarrior;
+    Mem_Exit myexit;
+    vector<Mem_Obstacle> myobstacles;
+    ScreenWin screen;
+    KeyboardWin keyboard;
 public:
-    Maze(); //缺省的构造函数
+    Maze(); 
     void maze_init();   
-    int move_warrior(int keyvalue);//根据键盘输入值，在屏幕上移动“勇士”
-                                   //返回值：1—移动；2—撞墙未动；3—撞障碍物牺牲；4-走出迷宫 
-    int move_obstacles();//在屏幕上自动移动“障碍物”  
+    int move_warrior(int keyvalue);
+                                   
+    int move_obstacles();
     void mem_clean();
     int maze_begin();
 };

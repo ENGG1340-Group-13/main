5eff183b87f548bfa5b10ffc770fda7ee5059df3
Booker-zgd <161011007+Booker-zgd@users.noreply.github.com>
2024-04-26 14:50:14 +0800

    Update keyboardwin.cpp

--- GP/keyboardwin.cpp
+++ GP/keyboardwin.cpp
@@ -2,47 +2,47 @@
 
 using namespace std;
 
-
+//initialize the function of handling the key input.
 KeyboardWin::KeyboardWin() {
     in = 0;
     listening=false;
 }
 
-/*等待键盘输入，直到等到有用的键返回*/
+//wait for the key input until it gets the useful keys.
 void KeyboardWin::get_key() {
-    char tmp;   //读取键值，或过滤功能键的第一个返回值
-    char tmp2;  //接受功能键
-    this->in=0; //清空以往记录 
+    char tmp; 
+    char tmp2;
+    this->in=0; 
     while (listening){
         tmp = getchar();     
-        if (tmp == '\033'){    //表示读取的是功能键或者方向键，丢掉第一个返回值，读取第二个返回值   
-            getchar(); // 忽略第二个字符 '[' 
-            switch (tmp2 = getchar()){   //接収功能键返回值          
-                case KEY_up://上 
+        if (tmp == '\033'){    
+            getchar(); 
+            switch (tmp2 = getchar()){     
+                case KEY_up:
                     this->in=KEY_up;
                     break;
-                case KEY_down://下 
+                case KEY_down:
                     this->in=KEY_down;
                     break;  
-                case KEY_left://左
+                case KEY_left:
                     this->in=KEY_left;
                     break;
-                case KEY_right://右
+                case KEY_right:
                     this->in=KEY_right;
                     break;
                 default:
                     this->in=0; 
             }
         }
-        else{   //普通按键，如字母、数字、space，Esc等按键      
+        else{     
             switch (tmp){
-                case KEY_SPACE://空格
+                case KEY_SPACE:
                     this->in=KEY_SPACE;                 
                     break;
-                case KEY_enter://Enter
+                case KEY_enter:
                     this->in=KEY_enter;
                     break;
-                case KEY_q://Esc
+                case KEY_q:
                     this->in=KEY_q;
                     break;
                 default: 
@@ -52,16 +52,19 @@ void KeyboardWin::get_key() {
     }
 }
 
+//the function that sets another thread to start getting key input.
 void KeyboardWin::begin_listening(){
     listening = true;
     kbd_thread = std::thread(&KeyboardWin::get_key, this);
     kbd_thread.detach();
 }
 
+//the function that stops the thread from getting key input.
 void KeyboardWin::stop_listening() {
     listening = false;    
 }
 
+//the function that actually read the key input.
 int KeyboardWin::read_key() {
     if (in != 0) {
         int return_in = in;

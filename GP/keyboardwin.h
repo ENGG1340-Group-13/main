8108e4fdc01b2a3f40cc9438547c1b081406ed9a
Booker-zgd <161011007+Booker-zgd@users.noreply.github.com>
2024-04-26 14:51:45 +0800

    Update keyboardwin.h

--- GP/keyboardwin.h
+++ GP/keyboardwin.h
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+//define the keys.
 #define KEY_up 'A'
 #define KEY_down 'B'
 #define KEY_left 'D'
@@ -13,7 +14,8 @@ using namespace std;
 #define KEY_enter '\n'
 #define KEY_esc '\033'
 #define KEY_q 'q'
- 
+
+//define the class of keyboardWin, with the viriables and function that handles key input.
 class KeyboardWin
 {
 public:
